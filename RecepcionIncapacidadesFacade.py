40ab9e596bb6aaa029d53541cac2a6bb9181fe54
brayanandrey <114691839+brayanandrey@users.noreply.github.com>
2023-11-23 15:22:49 -0500

    corrección de algunos errores

--- RecepcionIncapacidadesFacade.py
+++ RecepcionIncapacidadesFacade.py
@@ -17,21 +17,21 @@ class RecepcionIncapacidadesFacade:
         self.recepcionista.recibir_incapacidad(incapacidad)
         self.notificador.notificar_recepcion(incapacidad)
 
-    def verificar_incapacidades(self):
+    def verificar_incapacidades(self, incapacidad):
         self.verificador.verificar_incapacidades()
         self.notificador.notificar_verificacion()
         self.verificacion_documental.verificar_documentacion(incapacidad)
         
 
 # Ejemplo de uso
-facade = RecepcionIncapacidadesFacade()
-incapacidad1 = Incapacidad("Enfermedad General")
-incapacidad2 = Incapacidad("Accidente Laboral")
+if __name__ == "__main__":
+    facade = RecepcionIncapacidadesFacade()
+    incapacidad1 = Incapacidad("Enfermedad General")
+    incapacidad2 = Incapacidad("Accidente Laboral")
 
-facade.recibir_incapacidad(incapacidad1)
-facade.recibir_incapacidad(incapacidad2)
+    facade.recibir_incapacidad(incapacidad1)
+    facade.recibir_incapacidad(incapacidad2)
 
-facade.verificar_documentacion(incapacidad1)
-facade.verificar_documentacion(incapacidad2)
 
-facade.verificar_incapacidades()
+    facade.verificar_incapacidades(incapacidad1)
+    facade.verificar_incapacidades(incapacidad2)
---
+++ main.py
@@ -0,0 +1,13 @@
+import RecepcionIncapacidadesFacade
+import 
+
+facade = RecepcionIncapacidadesFacade()
+incapacidad1 = Incapacidad("Enfermedad General")
+incapacidad2 = Incapacidad("Accidente Laboral")
+
+facade.recibir_incapacidad(incapacidad1)
+facade.recibir_incapacidad(incapacidad2)
+
+
+facade.verificar_incapacidades(incapacidad1)
+facade.verificar_incapacidades(incapacidad2)
